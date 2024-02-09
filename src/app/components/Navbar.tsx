1c686c373d15ccfe45f5fde0416d149df9a58876
kenansabljakovic <kenan.sabljakovic@gmail.com>
2024-02-09 11:24:15 +0100

    styled HomeIcon and PortfolioGreyIcon based on the light/dark mode

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -23,16 +23,22 @@ export default function Navbar() {
     <div className="xl: max-w-[1440px] mx-auto mt-6 mb-[63px] px-[72px] flex justify-between">
       <div className="flex items-center gap-[10px] text-white font-bold">
         <CryptofolioLogoIcon />
-        <span className={`${inter.className} text-xl py-2`}>Cryptofolio</span>
+        <span
+          className={`${inter.className} dark:text-white text-[#353570] text-xl py-2`}
+        >
+          Cryptofolio
+        </span>
       </div>
       <div className="flex gap-14">
         <div className="flex items-center gap-[10px]">
           <HomeIcon />
-          <span className="text-white text-base font-medium">Home</span>
+          <span className="dark:text-white text-[#353570] text-base font-medium">
+            Home
+          </span>
         </div>
         <div className="flex items-center gap-[10px]">
           <PortfolioGreyIcon />
-          <span className="text-white opacity-50 text-base font-medium">
+          <span className="dark:text-white opacity-50 text-base font-medium">
             Portfolio
           </span>
         </div>
--- src/app/icons/PortfolioGreyIcon.tsx
+++ src/app/icons/PortfolioGreyIcon.tsx
@@ -1,3 +1,4 @@
+/*
 export const PortfolioGreyIcon = () => {
   return (
     <svg width="25" height="24" viewBox="0 0 25 24" fill="none">
@@ -11,5 +12,24 @@ export const PortfolioGreyIcon = () => {
         style={{ stroke: "white", strokeOpacity: 0.5 }}
       />
     </svg>
+  );
+};
+*/
+export const PortfolioGreyIcon = () => {
+  return (
+    <div className="w-6 h-6 flex items-center justify-center">
+      <svg
+        viewBox="0 0 25 24"
+        fill="none"
+        className="stroke-current dark:text-white opacity-50"
+      >
+        <path
+          d="M21.5 12L12.5 18L3.5 12M21.5 16L12.5 22L3.5 16M21.5 8L12.5 14L3.5 8L12.5 2L21.5 8Z"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </div>
   );
 };
