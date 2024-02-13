b974ea568e0af049eb6bd5a5c73f384034e4a1ec
kenansabljakovic <kenan.sabljakovic@gmail.com>
2024-02-13 08:20:12 +0100

    modiefied indicator as props in progress ui

--- src/app/components/ui/progress.tsx
+++ src/app/components/ui/progress.tsx
@@ -8,7 +8,7 @@ import { cn } from "@/lib/utils";
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
+>(({ className, value, indicator, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
@@ -18,7 +18,7 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className={`h-full w-full flex-1 ${props.indicator} transition-all`}
+      className={`h-full w-full flex-1 ${indicator} transition-all`}
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
