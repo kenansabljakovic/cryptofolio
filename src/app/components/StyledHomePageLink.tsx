973b9123076041b6d5316c872f5ccb1133c2e025
kenansabljakovic <kenan.sabljakovic@gmail.com>
2024-02-14 18:44:19 +0100

    created SyledHomePageLink component for links on the home page Coins and Converter to not repeat styles for every link

---
+++ src/app/components/StyledHomePageLink.tsx
@@ -0,0 +1,33 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type StyledHomePageLinkProps = {
+  href: string;
+  children: string;
+};
+
+export default function StyledHomePageLink({
+  href,
+  children,
+}: StyledHomePageLinkProps) {
+  const pathname = usePathname();
+
+  const baseStyle =
+    "w-[244px] h-[45px] flex items-center justify-center rounded-md text-base font-normal focus:outline-none";
+  const activeStyle =
+    "bg-[rgb(120,120,250,0.7)] border border-[#7878FA] text-white shadow-md";
+  const inactiveStyle =
+    "dark:bg-[#232336] dark:text-white text-[#424286] border-none";
+
+  return (
+    <Link
+      href={href}
+      className={`link ${
+        pathname === href ? activeStyle : inactiveStyle
+      } ${baseStyle}`}
+    >
+      {children}
+    </Link>
+  );
+}
