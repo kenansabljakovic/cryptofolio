7508aec3395f3694a49da158608d7b1a763cd8d3
bonz88 <kenan.sabljakovic@gmail.com>
2024-02-25 16:40:44 +0100

    added CarouselCoins component to page.tsx Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import Converter from "./converter/page";
 import NavHome from "./components/NavHome";
+import CarouselCoins from "./components/CarouselCoins";
 
 export default function Home() {
   return (
     <main>
       <NavHome />
+      <div className="mt-[70px] max-w-[1440px] mx-auto xl:px-[72px] lg:px-[36px] md:px-[24px]">
+        <CarouselCoins />
+      </div>
     </main>
   );
 }
