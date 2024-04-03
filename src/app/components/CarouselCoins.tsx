d18b81bffe8f7dce9cd777a7fc40c15a3ca73d47
kenansabljakovic <kenan.sabljakovic@gmail.com>
2024-04-03 08:30:54 +0200

    added variable coinSelected because expression selectedCoin === coin.id has been used more than once

--- src/app/components/CarouselCoins.tsx
+++ src/app/components/CarouselCoins.tsx
@@ -61,90 +61,93 @@ export default function CarouselCoins({
       className="w-full"
     >
       <CarouselContent className="-ml-1 sm:-ml-2">
-        {data?.map((coin) => (
-          <CarouselItem
-            key={coin.id}
-            className="pl-1 basis-1/4 sm:basis-1/2 sm:pl-2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5"
-          >
-            <button
-              onClick={() => handleCarousel(coin.id)}
-              className={`${
-                selectedCoin === coin.id
-                  ? "bg-[rgb(120,120,250,0.7)] border border-[#7878FA] shadow-md rounded-md"
-                  : "dark:bg-[#232336] sm:dark:bg-[#191925] bg-[rgb(255,255,255)]"
-              } px-2 py-2 w-full sm:h-[88px] sm:w-full flex items-center gap-1 sm:gap-4 rounded-lg cursor-pointer sm:px-4`}
+        {data?.map((coin) => {
+          const coinSelected = selectedCoin === coin.id;
+          return (
+            <CarouselItem
+              key={coin.id}
+              className="pl-1 basis-1/4 sm:basis-1/2 sm:pl-2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5"
             >
-              <Image
-                className="w-6 h-6 sm:w-8 sm:h-8"
-                src={coin.image}
-                alt={coin.name}
-                width={32}
-                height={32}
-              />
+              <button
+                onClick={() => handleCarousel(coin.id)}
+                className={`${
+                  coinSelected
+                    ? "bg-[rgb(120,120,250,0.7)] border border-[#7878FA] shadow-md rounded-md"
+                    : "dark:bg-[#232336] sm:dark:bg-[#191925] bg-[rgb(255,255,255)]"
+                } px-2 py-2 w-full sm:h-[88px] sm:w-full flex items-center gap-1 sm:gap-4 rounded-lg cursor-pointer sm:px-4`}
+              >
+                <Image
+                  className="w-6 h-6 sm:w-8 sm:h-8"
+                  src={coin.image}
+                  alt={coin.name}
+                  width={32}
+                  height={32}
+                />
 
-              <div className="flex sm:flex-col items-center sm:items-start gap-4 sm:gap-0">
-                <div className="text-[16px] dark:text-white text-[#181825] font-medium leading-6 text-left">
-                  <span
-                    className={`${
-                      selectedCoin === coin.id && "text-white"
-                    } hidden sm:inline`}
-                  >
-                    {coin.name}
-                  </span>
-                  <span
-                    className={`hidden sm:inline sm:ml-1 ${
-                      selectedCoin === coin.id && "text-white"
-                    }`}
-                  >
-                    (
-                  </span>
-                  <span
-                    className={`${
-                      selectedCoin === coin.id && "text-white"
-                    } uppercase text-sm font-medium sm:text-base`}
-                  >
-                    {coin.symbol}
-                  </span>
-                  <span
-                    className={`hidden sm:inline ${
-                      selectedCoin === coin.id && "text-white"
-                    }`}
-                  >
-                    )
-                  </span>
-                </div>
-                <div className="hidden sm:flex sm:flex-row sm:items-center sm:gap-2">
-                  <span
-                    className={`${
-                      selectedCoin === coin.id && "text-white"
-                    } dark:text-[#D1D1D1] text-[#424286] text-sm font-normal leading-4`}
-                  >
-                    {currencySymbol}
-                    {coin.current_price}
-                  </span>
-                  <div className="flex items-center gap-1">
-                    <span className="">
-                      {coin.price_change_percentage_24h > 0 ? (
-                        <ChevronUpIcon />
-                      ) : (
-                        <ChevronDownIcon />
-                      )}
+                <div className="flex sm:flex-col items-center sm:items-start gap-4 sm:gap-0">
+                  <div className="text-[16px] dark:text-white text-[#181825] font-medium leading-6 text-left">
+                    <span
+                      className={`${
+                        coinSelected && "text-white"
+                      } hidden sm:inline`}
+                    >
+                      {coin.name}
                     </span>
                     <span
-                      className={`text-sm font-normal ${
-                        coin.price_change_percentage_24h < 0
-                          ? "text-[#FD2263]"
-                          : "text-[#00F0E2]"
+                      className={`hidden sm:inline sm:ml-1 ${
+                        coinSelected && "text-white"
                       }`}
                     >
-                      {Math.abs(coin.price_change_percentage_24h).toFixed(2)}%
+                      (
+                    </span>
+                    <span
+                      className={`${
+                        coinSelected && "text-white"
+                      } uppercase text-sm font-medium sm:text-base`}
+                    >
+                      {coin.symbol}
+                    </span>
+                    <span
+                      className={`hidden sm:inline ${
+                        coinSelected && "text-white"
+                      }`}
+                    >
+                      )
                     </span>
                   </div>
+                  <div className="hidden sm:flex sm:flex-row sm:items-center sm:gap-2">
+                    <span
+                      className={`${
+                        coinSelected && "text-white"
+                      } dark:text-[#D1D1D1] text-[#424286] text-sm font-normal leading-4`}
+                    >
+                      {currencySymbol}
+                      {coin.current_price}
+                    </span>
+                    <div className="flex items-center gap-1">
+                      <span className="">
+                        {coin.price_change_percentage_24h > 0 ? (
+                          <ChevronUpIcon />
+                        ) : (
+                          <ChevronDownIcon />
+                        )}
+                      </span>
+                      <span
+                        className={`text-sm font-normal ${
+                          coin.price_change_percentage_24h < 0
+                            ? "text-[#FD2263]"
+                            : "text-[#00F0E2]"
+                        }`}
+                      >
+                        {Math.abs(coin.price_change_percentage_24h).toFixed(2)}%
+                      </span>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </button>
-          </CarouselItem>
-        ))}
+              </button>
+            </CarouselItem>
+          );
+        })}
       </CarouselContent>
       <CarouselPrevious className="hidden dark:bg-[#6161D6] dark:bg-opacity-50 bg-[#6161D6] bg-opacity-50 dark:hover:bg-[#6161D6] hover:bg-[#6161D6] hover:bg-opacity-70" />
       <CarouselNext className="hidden sm:inline-flex dark:bg-[#6161D6] dark:bg-opacity-50 bg-[#6161D6] bg-opacity-50 dark:hover:bg-[#6161D6] hover:bg-[#6161D6] hover:bg-opacity-70" />
