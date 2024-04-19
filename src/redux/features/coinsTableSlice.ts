d4a577bcf42fe96eb1189ebb65a0a14b5ee5276d
kenansabljakovic <kenan.sabljakovic@gmail.com>
2024-04-19 08:55:20 +0200

    removed console.log

--- src/redux/features/coinsTableSlice.ts
+++ src/redux/features/coinsTableSlice.ts
@@ -60,7 +60,6 @@ const initialState: CoinMarketData = {
 export const getCoinMarketData = createAsyncThunk(
   "coinMarket/getCoinMarketData",
   async ({ currency, page }: GetCoinMarketDataArgs, { rejectWithValue }) => {
-    console.log(`Fetching data for page: ${page}`);
     try {
       const response = await fetch(
         `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=20&page=${page}&sparkline=true&price_change_percentage=1h%2C24h%2C7d&x_cg_demo_api_key=${process.env.NEXT_PUBLIC_API_KEY}`
