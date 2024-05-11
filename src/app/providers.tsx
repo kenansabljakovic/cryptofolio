bfb1494b34bc21de146d644a5f6af8555e8bca9d
kenansabljakovic <kenan.sabljakovic@gmail.com>
2024-05-11 21:08:11 +0200

    Wrapped NextUI provider inside providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,11 +2,13 @@
 
 import { ThemeProvider } from "next-themes";
 import { ReduxProvider } from "../redux/provider";
+import { NextUIProvider } from "@nextui-org/react";
+
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ReduxProvider>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        {children}
+        <NextUIProvider>{children}</NextUIProvider>
       </ThemeProvider>
     </ReduxProvider>
   );
