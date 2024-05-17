6c3c338fca04e51e0b2c92a202d6b804d4dfffdc
bonz88 <kenan.sabljakovic@gmail.com>
2024-05-17 15:51:34 +0200

    added route for portfolio page

---
+++ src/app/portfolio/layout.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type PortfolioProps = {
+  children: React.ReactNode;
+};
+
+export default function PortfolioLayout({ children }: PortfolioProps) {
+  return <>{children}</>;
+}
---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,7 @@
+export default function PortfolioPage() {
+  return (
+    <main className="max-w-[1440px] mx-auto xl:px-[72px] lg:px-[36px] px-[24px] mt-5 sm:mt-10 pb-10">
+      <h1>Portfolio page</h1>
+    </main>
+  );
+}
