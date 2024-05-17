a998773c8e868a0ee9428c0a2042389438208a9d
bonz88 <kenan.sabljakovic@gmail.com>
2024-05-17 15:52:21 +0200

    renamed component to PortfolioIcon

--- src/app/icons/PortfolioIcon.tsx
+++ src/app/icons/PortfolioIcon.tsx
@@ -1,10 +1,14 @@
-export const PortfolioGreyIcon = () => {
+export const PortfolioIcon = ({ isActive }: { isActive: boolean }) => {
   return (
-    <div className="w-6 h-6 flex items-center justify-center">
+    <div
+      className={`w-6 h-6 flex items-center justify-center ${
+        !isActive && "opacity-50"
+      }`}
+    >
       <svg
         viewBox="0 0 25 24"
         fill="none"
-        className="stroke-current dark:text-white opacity-50"
+        className="stroke-current dark:text-white"
       >
         <path
           d="M21.5 12L12.5 18L3.5 12M21.5 16L12.5 22L3.5 16M21.5 8L12.5 14L3.5 8L12.5 2L21.5 8Z"
